2e6c133d2f21bd943642adc0792446c6c5c82945
pabloroque1 <pabloroque1995@gmail.com>
2021-09-27 16:52:00 -0300

    creacion de funciones renderizadores de formulario y home

---
+++ src/formPage.ts
@@ -0,0 +1,33 @@
+export function formPage(){
+    return `
+        <div class="titulo-contacto">
+            <h3>Contacto</h3>
+        </div>
+        <form action="" class="form-contacto" id="formulario">
+            <div>
+                <label for="">Nombre</label>
+                <input type="text" id="nombre">
+            </div>
+            
+            <div>
+                <label for="">Apellido</label>
+                <input type="text" id="apellido">
+            </div>
+            
+            <div>
+                <label for="">Email</label>
+                <input type="email" id="email" required>
+            </div>
+
+            <div>
+                <label for="">Consulta</label>
+                <textarea name="" id="" cols="18" rows="10" id="consulta"></textarea>
+            </div>
+
+            <div>
+                <input type="submit" value="Enviar">
+            </div>
+        
+        </form> 
+    `;
+};
