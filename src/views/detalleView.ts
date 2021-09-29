d62483508a5e5d93211017f782263bdcaf460d8b
pabloroque1 <pabloroque1995@gmail.com>
2021-09-29 11:15:44 -0300

    creacion de carpeta views, para las vistas

---
+++ src/views/detalleView.ts
@@ -0,0 +1,28 @@
+import { Producto } from "../models/producto";
+
+export function detalleItemView(product: Producto){
+    return `
+        <div>
+            <img src="${product.img}" alt="">
+        </div>
+
+        <div class="caracteristicas">
+            <p>
+                Notebook Asus Zenbook 14 UX425EA-BM004T 
+                Intel Core i5 1135G7 8Gb SSD 512Gb 14" 
+                Win10 
+            </p>
+        </div>
+
+        <div class="descripcion">
+            <h2>Descripcion</h2>
+            <p>
+                ${product.descripcion}
+            </p>
+        </div>
+
+        <div class="boton">
+            <button>Comprar</button>
+        </div>
+    `;
+};
---
+++ src/views/productItem.ts
@@ -0,0 +1,14 @@
+import { Producto } from "../models/producto";
+
+export function productItemView(product: Producto){
+    return `
+        <div class="caja" id="${product.id}">
+            <a href="/detalleproducto" class="enlace-detalle"><img src="${product.img}" alt=""></a>
+            <h3>$${product.precio}</h3>
+            <p>
+                ${product.nombre}
+            </p>
+            <input type="submit" value="Agregar" class="btn-agregar">
+        </div>
+    `;
+};
