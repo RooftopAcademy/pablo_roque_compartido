72563d217d8206147e33e1dd4f3587487fc6c872
pabloroque1 <pabloroque1995@gmail.com>
2021-09-29 11:36:50 -0300

    modificacion del nombre del boton de redes sociales

--- src/helpers/boton-menu.ts
+++ src/helpers/boton-menu.ts
@@ -2,7 +2,7 @@ const btnMenu = document.querySelector("#menu") as HTMLElement;
 const menuContent = document.querySelector(".menu-content") as HTMLElement;
 
 
-export function Menu(){
+export function btnRedesSociales(){
     btnMenu.addEventListener('click',function(): void{
         menuContent.classList.toggle('menu-active');
     })
