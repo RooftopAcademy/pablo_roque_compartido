e43979970edb532758c0582f9cd1e35438e34d2f
pabloroque1 <pabloroque1995@gmail.com>
2021-09-29 11:16:38 -0300

    creacion de carpeta interfaces, y servicios

---
+++ src/services/userApi.ts
@@ -0,0 +1,26 @@
+import { User } from "../models/user"
+import UsuarioInterface from "../interfaces/userInterface"
+
+
+export let users: User[] = []
+
+
+
+export async function getUsers(url: string){
+    const respuesta = await fetch(url)
+                    .then(response => response.json())
+                    .then(data => pushUsers(data))
+                    .catch(err => console.log(err))
+}
+
+
+const pushUsers = (respuesta: UsuarioInterface[]) =>{
+    for (let index = 0; index < respuesta.length; index++) {
+        users.push(respuesta[index]);
+    }
+//   let body = document.getElementById('list-usuarios')
+//   for (let index = 0; index < data.length; index++) {
+//     body.innerHTML += usuarioView(data[index])  
+//   }
+}
+
